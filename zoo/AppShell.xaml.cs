8b35aaa624ce224c285fd8e950f51e8f1fb9e7cc
sashok246801 <buzin.sasha.2003@gmail.com>
2022-09-23 11:48:47 +0300

    Добавьте файлы проекта.

---
+++ zoo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace zoo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ zoo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace zoo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
